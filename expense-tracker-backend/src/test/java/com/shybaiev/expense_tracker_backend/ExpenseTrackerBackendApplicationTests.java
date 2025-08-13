3dfea96849d0a0b9267e89046a7dbdf787e39062
AndriiShybaiev <60670387+AndriiShybaiev@users.noreply.github.com>
2025-08-13 12:07:38 +0200

    Initial commit

---
+++ expense-tracker-backend/src/test/java/com/shybaiev/expense_tracker_backend/ExpenseTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shybaiev.expense_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

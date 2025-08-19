f627628fbbd12f7601d244e9ed1d0a867f69a683
AndriiShybaiev <60670387+AndriiShybaiev@users.noreply.github.com>
2025-08-19 20:30:08 +0200

    Configure H2 database for testing and update CI pipeline
    - Added H2 database dependency for testing in `pom.xml`.
    - Created `application-test.properties` to configure H2 for tests.
    - Updated CI pipeline to use `mvn clean verify` for comprehensive testing.
    - Refined IntelliJ project file for dependency duplication.
    - Activated test profile in `ExpenseTrackerBackendApplicationTests`.

--- expense-tracker-backend/src/test/java/com/shybaiev/expense_tracker_backend/ExpenseTrackerBackendApplicationTests.java
+++ expense-tracker-backend/src/test/java/com/shybaiev/expense_tracker_backend/ExpenseTrackerBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.shybaiev.expense_tracker_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExpenseTrackerBackendApplicationTests {
 
 	@Test

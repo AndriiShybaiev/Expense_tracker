073f01fbf3b0c6bfc44d8ae71c10f19a35106c0a
AndriiShybaiev <60670387+AndriiShybaiev@users.noreply.github.com>
2025-08-23 18:24:32 +0200

    Link `Budget` and `Expense` entities with JPA mappings and introduce validation annotations
    - Added `@OneToMany` relationship in `Budget` entity and linked it with `Expense`.
    - Configured `@ManyToOne` in `Expense` to associate it with `Budget`.
    - Introduced validation annotations for `Expense` fields (e.g., `@NotNull`, `@DecimalMin`).

--- expense-tracker-backend/src/main/java/com/shybaiev/expense_tracker_backend/entity/Budget.java
+++ expense-tracker-backend/src/main/java/com/shybaiev/expense_tracker_backend/entity/Budget.java
@@ -1,4 +1,28 @@
 package com.shybaiev.expense_tracker_backend.entity;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Budget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(
+            mappedBy = "budget",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+    )
+    @JsonManagedReference
+    @Valid
+    private List<Expense> expenses = new ArrayList<>();
 }
--- expense-tracker-backend/src/main/java/com/shybaiev/expense_tracker_backend/entity/Expense.java
+++ expense-tracker-backend/src/main/java/com/shybaiev/expense_tracker_backend/entity/Expense.java
@@ -1,17 +1,45 @@
-// Expense.java
+
 package com.shybaiev.expense_tracker_backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Column()
+    private String description;
+
+    @Column(length = 100)
+    private String place;
+
+    @Column()
+    private OffsetDateTime timestamp;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "budget_id")
+    private Budget budget;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)

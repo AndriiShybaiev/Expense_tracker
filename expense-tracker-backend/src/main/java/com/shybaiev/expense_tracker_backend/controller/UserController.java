867d9fc121bb7e764f3c33a3cd3e6919b479ee8c
AndriiShybaiev <60670387+AndriiShybaiev@users.noreply.github.com>
2025-09-01 19:50:58 +0200

    Add `getUserById` endpoint to `UserController`
    - Introduced `/users/{id}` endpoint for retrieving user details by ID.
    - Returns `UserDto` if the user exists, otherwise responds with `404 Not Found`.

--- expense-tracker-backend/src/main/java/com/shybaiev/expense_tracker_backend/controller/UserController.java
+++ expense-tracker-backend/src/main/java/com/shybaiev/expense_tracker_backend/controller/UserController.java
@@ -3,12 +3,14 @@ import com.shybaiev.expense_tracker_backend.dto.UserDto;
 import com.shybaiev.expense_tracker_backend.entity.User;
 import com.shybaiev.expense_tracker_backend.mapper.UserMapper;
 import com.shybaiev.expense_tracker_backend.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -17,6 +19,16 @@ public class UserController {
 
     private final UserService userService;
     private final UserMapper userMapper;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        Optional<User> maybeUser = userService.getUserById(id);
+        if (maybeUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        UserDto body = userMapper.toDto(maybeUser.get());
+        return ResponseEntity.ok(body);
+    }
 
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers() {
